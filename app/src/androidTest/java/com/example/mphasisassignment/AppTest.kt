ffb6d9ddc461f1083cc6c3580e6154758a772e05
kirti <kashyap.kirti11@gmail.com>
2021-04-05 23:23:12 +0100

    display album fragment

---
+++ app/src/androidTest/java/com/example/mphasisassignment/AppTest.kt
@@ -0,0 +1,34 @@
+package com.example.mphasisassignment
+
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class AppTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Test
+    fun checkPath() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // Check MainActivity
+        onView(withId(R.id.nav_host_fragment)).check(matches(isDisplayed()))
+        // Check Album fragment screen is displayed
+        onView(withId(R.id.recyclerView)).check(matches(isDisplayed()))
+
+    }
+}
